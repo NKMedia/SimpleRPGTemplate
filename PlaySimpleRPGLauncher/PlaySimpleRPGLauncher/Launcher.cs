26242fed6862956eb117af6b2f6f68f3760a2aae
Nico Kuechler <mail@nico-kuechler.de>
2016-12-27 12:42:44 +0100

    Added a simple Game Launcher Applikation to the project
    Added a custom Game Launcher Applikation to customise launching of the
    game in the future.
    Added nkmedia_simplerpg_update.html to provide sample content for the
    launcher app.

---
+++ PlaySimpleRPGLauncher/PlaySimpleRPGLauncher/Launcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PlaySimpleRPGLauncher
+{
+    class Launcher
+    {
+        public static void PlayGame(string gamename_exe)
+        {
+            //Start game by the passed gamename.exe
+            Process.Start(gamename_exe);
+
+            //Close this Launcher Application
+            Environment.Exit(0);
+        }
+
+        public static void LaunchWebsite(string url)
+        {
+            //Opens the devault browser tap with the given url
+            Process.Start(url);
+        }
+    }
+}
