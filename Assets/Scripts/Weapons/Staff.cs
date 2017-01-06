7b905c0d697d0910ec6099c0b0cd5acc89e401f3
Nico Kuechler <mail@nico-kuechler.de>
2017-01-06 10:18:38 +0100

    Preparation for projectiles
    added the_willow_wand
    modified lighting setup to give a better reqresentation in the editor
    added fireball.cs
    added staff.cs

---
+++ Assets/Scripts/Weapons/Staff.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NKM.RPGFramework
+{
+    public class Staff: MonoBehaviour, IWeapon
+    {
+        private Animator animator;
+        public List<BaseStat> Stats{ get; set; }
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void PerformAttack()
+        {
+            Debug.Log(this.name + " performes a base attack!");
+            animator.SetTrigger("Base_Attack");
+        }
+
+        public void PerformSpecialAttack()
+        {
+            Debug.Log(this.name + "performes a special attack!");
+            animator.SetTrigger("Special_Attack");
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Enemy")
+            {
+                other.GetComponent<IEnemy>().TakeDamage(2);
+            }
+        }
+
+    }
+}
